d2eecbe5891b2df41894907c0f180eebe54768e1
TaylorY1 <YockeyT139@gmail.com>
2022-05-24 13:32:40 -0600

    Configure hibernate, tested entity

---
+++ DB/foodtruckdb.sql
@@ -0,0 +1,157 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema foodtruckdb
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `foodtruckdb` ;
+
+-- -----------------------------------------------------
+-- Schema foodtruckdb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `foodtruckdb` DEFAULT CHARACTER SET utf8 ;
+USE `foodtruckdb` ;
+
+-- -----------------------------------------------------
+-- Table `food_truck`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `food_truck` ;
+
+CREATE TABLE IF NOT EXISTS `food_truck` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  `description` VARCHAR(200) NULL,
+  `rating` INT NULL,
+  `category` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `user`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `user` ;
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(200) NOT NULL,
+  `enabled` TINYINT NULL,
+  `role` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `food_category`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `food_category` ;
+
+CREATE TABLE IF NOT EXISTS `food_category` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `category` VARCHAR(55) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `menu`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `menu` ;
+
+CREATE TABLE IF NOT EXISTS `menu` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(50) NOT NULL,
+  `description` VARCHAR(45) NULL,
+  `price` DECIMAL(4,2) NULL,
+  `image_url` VARCHAR(100) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `comment`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `comment` ;
+
+CREATE TABLE IF NOT EXISTS `comment` (
+  `comment` VARCHAR(300) NOT NULL,
+  `user_id` INT NULL,
+  PRIMARY KEY (`comment`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `favorites`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `favorites` ;
+
+CREATE TABLE IF NOT EXISTS `favorites` (
+  `user_id` INT NOT NULL,
+  `food_truck_id` INT NOT NULL,
+  PRIMARY KEY (`user_id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `location`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `location` ;
+
+CREATE TABLE IF NOT EXISTS `location` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `coordinates` DECIMAL(12,2) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `tagged_truck`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `tagged_truck` ;
+
+CREATE TABLE IF NOT EXISTS `tagged_truck` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(55) NULL,
+  `website_url` VARCHAR(55) NULL,
+  `photo_url` VARCHAR(55) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `schedule`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `schedule` ;
+
+CREATE TABLE IF NOT EXISTS `schedule` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `arrival` DATETIME NULL,
+  `departure` DATETIME NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+SET SQL_MODE = '';
+DROP USER IF EXISTS foodtruckuser@localhost;
+SET SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+CREATE USER 'foodtruckuser'@'localhost' IDENTIFIED BY 'foodtruck';
+
+GRANT SELECT, INSERT, TRIGGER, UPDATE, DELETE ON TABLE * TO 'foodtruckuser'@'localhost';
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+-- -----------------------------------------------------
+-- Data for table `user`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `foodtruckdb`;
+INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `role`) VALUES (1, 'admin', 'x', 1, 'ROLE_ADMIN');
+
+COMMIT;
+
