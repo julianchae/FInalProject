77513078f8915042d0120cc22ad43d3f750b2f3c
Julian Chae <Julianchae9@gmail.com>
2022-05-24 11:00:44 -0600

    Create and config Spring Boot, add JPA proj dependency

---
+++ FoodTruckTracker/src/main/java/com/skilldistillery/foodtruck/FoodTruckTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.foodtruck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodTruckTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodTruckTrackerApplication.class, args);
+	}
+
+}
---
+++ FoodTruckTracker/src/test/java/com/skilldistillery/foodtruck/FoodTruckTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.foodtruck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodTruckTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
