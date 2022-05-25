2b5f22247d016a2a156cfaf5014231beee67ba36
Julian Chae <Julianchae9@gmail.com>
2022-05-25 14:26:53 -0600

    xml updated, and added taggedTruck

--- JPAFoodTruckTracker/src/main/java/com/skilldistillery/foodtruck/entities/Order.java
+++ JPAFoodTruckTracker/src/main/java/com/skilldistillery/foodtruck/entities/Order.java
@@ -7,8 +7,10 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "pre_order")
 public class Order {
 	
 	@Id
--- JPAFoodTruckTracker/src/main/java/com/skilldistillery/foodtruck/entities/TaggedTruck.java
+++ JPAFoodTruckTracker/src/main/java/com/skilldistillery/foodtruck/entities/TaggedTruck.java
@@ -1,5 +1,88 @@
 package com.skilldistillery.foodtruck.entities;
 
+import java.time.LocalDate;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class TaggedTruck {
 
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	@Column(name="photo_url")
+	private String photoUrl;
+	
+	private String comment;
+	@Column(name="date_tagged")
+	private LocalDate dateTagged;
+	
+	//TODO: add mappings for food truck, location, user
+	public TaggedTruck() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getPhotoUrl() {
+		return photoUrl;
+	}
+
+	public void setPhotoUrl(String photoUrl) {
+		this.photoUrl = photoUrl;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public LocalDate getDateTagged() {
+		return dateTagged;
+	}
+
+	public void setDateTagged(LocalDate dateTagged) {
+		this.dateTagged = dateTagged;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TaggedTruck other = (TaggedTruck) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "TaggedTruck [id=" + id + ", photoUrl=" + photoUrl + ", comment=" + comment + ", dateTagged="
+				+ dateTagged + "]";
+	}
+	
+	
+	
+	
 }
