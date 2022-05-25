750bae7da326df1030339a05469ecb325353ab6c
Julian Chae <Julianchae9@gmail.com>
2022-05-25 14:13:01 -0600

    order entity

--- JPAFoodTruckTracker/src/main/java/com/skilldistillery/foodtruck/entities/Order.java
+++ JPAFoodTruckTracker/src/main/java/com/skilldistillery/foodtruck/entities/Order.java
@@ -1,5 +1,59 @@
 package com.skilldistillery.foodtruck.entities;
 
-public class Order {
+import java.time.LocalDate;
+import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Order {
+	
+	@Id
+	@GeneratedValue
+	private int id;
+	@Column(name="ordered_date")
+	private LocalDate orderedDate;
+	@Column(name="pickup_date")
+	private LocalDate pickupDate;
+	@Column(name="special_requests")
+	private String specialRequests;
+	private boolean completed;
+	
+	//TODO: Map out User, menu
+	
+	
+	public Order() {
+		super();
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "Order [id=" + id + ", orderedDate=" + orderedDate + ", pickupDate=" + pickupDate + ", specialRequests="
+				+ specialRequests + ", completed=" + completed + "]";
+	}
+	
+	
+	
+
+	
 }
