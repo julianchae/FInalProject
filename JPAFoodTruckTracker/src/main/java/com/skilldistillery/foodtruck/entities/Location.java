67f298814a8f59ae74c8a283fd5b898d0a351d14
parisasledge-source <parisasledge@me.com>
2022-05-25 15:43:03 -0400

    User, Location & Comment Entities Done

--- JPAFoodTruckTracker/src/main/java/com/skilldistillery/foodtruck/entities/Location.java
+++ JPAFoodTruckTracker/src/main/java/com/skilldistillery/foodtruck/entities/Location.java
@@ -1,5 +1,89 @@
 package com.skilldistillery.foodtruck.entities;
 
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+//mysql> desc location;
+//+--------+-------------+------+-----+---------+----------------+
+//| Field  | Type        | Null | Key | Default | Extra          |
+//+--------+-------------+------+-----+---------+----------------+
+//| id     | int(11)     | NO   | PRI | NULL    | auto_increment |
+//| street | varchar(55) | YES  |     | NULL    |                |
+//| city   | varchar(65) | YES  |     | NULL    |                |
+//| state  | varchar(2)  | YES  |     | NULL    |                |
+//| zip    | varchar(10) | YES  |     | NULL    |                |
+//+--------+-------------+------+-----+---------+----------------+
+
+@Entity
 public class Location {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String street;
+	private String city;
+	private String state;
+	private String zip;
+	
+	public Location() {
+		super();
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getZip() {
+		return zip;
+	}
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+	@Override
+	public String toString() {
+		return "Location [id=" + id + ", street=" + street + ", city=" + city + ", state=" + state + ", zip=" + zip
+				+ "]";
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Location other = (Location) obj;
+		return id == other.id;
+	}
+	
+	
 
 }
