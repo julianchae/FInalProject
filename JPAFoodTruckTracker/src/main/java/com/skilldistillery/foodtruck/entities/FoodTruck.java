1f50da31675c7736f1a1aed08bf4041810a0c101
Julian Chae <Julianchae9@gmail.com>
2022-05-25 13:43:57 -0600

    entities stubbed for festival and foodtruck

--- JPAFoodTruckTracker/src/main/java/com/skilldistillery/foodtruck/entities/FoodTruck.java
+++ JPAFoodTruckTracker/src/main/java/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -1,5 +1,126 @@
 package com.skilldistillery.foodtruck.entities;
 
-public class FoodTruck {
+import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+@Entity
+public class FoodTruck {
+	
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	private String name;
+	
+	private String description;
+	
+	@Column(name = "img_url")
+	private String imgUrl;
+	
+	private boolean active;
+	@Column(name = "date_created")
+	private String dateCreated;
+	@Column(name = "websiteUrl")
+	private String websiteUrl;
+	
+	
+	//TODO: add mappings for User, food category, comment, tagged truck, request, menu item, schedule
+	public FoodTruck() {
+		super();
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+
+	public boolean isActive() {
+		return active;
+	}
+
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+
+	public String getDateCreated() {
+		return dateCreated;
+	}
+
+
+	public void setDateCreated(String dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
+
+	public String getWebsiteUrl() {
+		return websiteUrl;
+	}
+
+
+	public void setWebsiteUrl(String websiteUrl) {
+		this.websiteUrl = websiteUrl;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FoodTruck other = (FoodTruck) obj;
+		return id == other.id;
+	}
+	
+	
+	
 }
