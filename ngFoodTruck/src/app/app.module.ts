dedb10297c43392fb5515bc22af69b9aecb99388
TaylorY1 <YockeyT139@gmail.com>
2022-05-26 15:02:43 -0600

    Register functioning

--- ngFoodTruck/src/app/app.module.ts
+++ ngFoodTruck/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
@@ -9,12 +10,13 @@ import { RegisterComponent } from './components/register/register.component';
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ngFoodTruck/src/app/models/user.ts
+++ ngFoodTruck/src/app/models/user.ts
@@ -4,12 +4,22 @@ export class User {
   username: string | null;
   password: string |null;
   email: string | null;
+  enabled: boolean | null;
+  role: string| null;
+  firstName: string | null;
+  lastName: string |null;
+  imgUrl: string | null;
 
   constructor(
     id: number | null = 0,
     username: string | null = '',
     password: string |null = '',
-    email: string |null = ''
+    email: string |null = '',
+    enabled: boolean | null = true,
+    role: string| null = '',
+    firstName: string | null = '',
+    lastName: string |null = '',
+    imgUrl: string | null = ''
   )
 
   {
@@ -17,6 +27,11 @@ export class User {
     this.username = username;
     this.password = password;
     this.email = email;
+    this.enabled = enabled;
+    this.role = role;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.imgUrl = imgUrl;
   }
 
 
