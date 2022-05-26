77677499091e2d128b3d11391188a737f205b8ce
Julian Chae <Julianchae9@gmail.com>
2022-05-26 12:15:08 -0600

    got rid of comment

--- FoodTruckTracker/src/main/java/com/skilldistillery/foodtruck/controllers/TruckController.java
+++ FoodTruckTracker/src/main/java/com/skilldistillery/foodtruck/controllers/TruckController.java
@@ -14,7 +14,7 @@ import com.skilldistillery.foodtruck.services.TruckService;
 @RestController
 @RequestMapping("api")
 public class TruckController {
-	//TruckController
+	
 	
 	@Autowired
 	private TruckService truckServe;
