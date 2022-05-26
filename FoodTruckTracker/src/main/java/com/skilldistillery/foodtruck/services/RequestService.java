3e7993272c56e8238500dbb991d4ddab21dec5ce
parisasledge-source <parisasledge@me.com>
2022-05-26 19:56:41 -0400

    Rest API Created For Request

---
+++ FoodTruckTracker/src/main/java/com/skilldistillery/foodtruck/services/RequestService.java
@@ -0,0 +1,20 @@
+package com.skilldistillery.foodtruck.services;
+
+import java.util.List;
+
+import com.skilldistillery.foodtruck.entities.Request;
+
+public interface RequestService {
+	
+	
+	public List<Request> index(String username);
+
+    public Request show(String username, int rid);
+
+    public Request create(String username, Request request);
+
+    public Request update(String username, int rid, Request request);
+
+    public boolean destroy(String username, int rid);
+
+}
