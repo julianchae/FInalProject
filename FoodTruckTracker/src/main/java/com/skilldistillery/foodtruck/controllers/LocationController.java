70ca6f69b9c21fbeedc3382d56f089c15ee1548a
parisasledge-source <parisasledge@me.com>
2022-05-28 16:33:05 -0400

    CRUD for Location is Done

--- FoodTruckTracker/src/main/java/com/skilldistillery/foodtruck/controllers/LocationController.java
+++ FoodTruckTracker/src/main/java/com/skilldistillery/foodtruck/controllers/LocationController.java
@@ -1,15 +1,22 @@
 package com.skilldistillery.foodtruck.controllers;
 
+import java.security.Principal;
+
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.skilldistillery.foodtruck.entities.Location;
+import com.skilldistillery.foodtruck.entities.Request;
 import com.skilldistillery.foodtruck.services.LocationService;
 
 //TODO: Needs Testing in Postman
@@ -29,4 +36,26 @@ public class LocationController {
 		  }
 		  return loc;
 	  }
+	 
+		@PostMapping("locations")
+		public Location addLocation(Principal principal, @RequestBody Location location) {
+			System.out.println(location.getCity());
+			System.out.println(location.getStreet());
+//			System.out.println(location);
+			return serv.create(principal.getName(), location);
+		}
+		
+		@PutMapping("locations/{lid}")
+		public Location updateLocation(Principal principal, @RequestBody Location location, @PathVariable int lid) {
+			return serv.update(principal.getName(), lid, location);
+		}
+//		
+//		@DeleteMapping("locations/{lid}")
+//		public void delete(Principal principal, HttpServletResponse resp, @PathVariable int lid) {
+//			if(serv.delete(principal.getName(), lid)) {
+//				resp.setStatus(204);
+//			} else {
+//				resp.setStatus(404);
+//			}
+//		}
 }
