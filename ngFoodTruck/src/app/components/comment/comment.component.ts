ae7e273c27fe5e523679253aa00c81a056e274af
Julian Chae <Julianchae9@gmail.com>
2022-05-30 15:26:44 -0600

    comments

--- ngFoodTruck/src/app/components/comment/comment.component.ts
+++ ngFoodTruck/src/app/components/comment/comment.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Comment } from './../../models/comment';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FoodTruck } from 'src/app/models/food-truck';
+import { User } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth.service';
+import { CommentService } from 'src/app/services/comment.service';
+
 
 @Component({
   selector: 'app-comment',
@@ -7,9 +14,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommentComponent implements OnInit {
 
-  constructor() { }
+  @Input() truck: FoodTruck = new FoodTruck();
+ comments : Comment [] = [];
+  newComment: Comment = new Comment();
+  user: User | null = null;
+  comment: Comment = new Comment();
+
+  constructor(private commentSvc: CommentService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.loadCommentsForTruck();
+    this.authService.getLoggedInUser().subscribe({
+      next:(user) => {
+        this.user = Object.assign({},user);
+        console.log(user);
+        // user1 = user;
+        // return user;
+      },
+      error:(fail) => {
+        console.error(fail);
+        // return null;
+      }
+    });
   }
 
+  loadCommentsForTruck(){
+    this.commentSvc.getCommentsOnTruck(this.truck.id).subscribe(
+      success => this.comments = success,
+      err => console.log("Observable got an error " + err)
+    );
+  }
+
+  createCommentOnTruck(){
+this.commentSvc.createCommentOnTruck(this.truck.id, this.comment).subscribe(
+  data => {
+    this.loadCommentsForTruck();
+    this.newComment = new Comment();
+  },
+  err => console.log("Observable got an error " + err)
+  );
+
+
+
+  }
 }
