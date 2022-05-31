a3fad8c08116d720c7dba0a081371025309a04b2
TaylorY1 <YockeyT139@gmail.com>
2022-05-31 17:50:39 -0600

    Food Truck Owner can see extra details of food truck

--- ngFoodTruck/src/app/components/profile/profile.component.ts
+++ ngFoodTruck/src/app/components/profile/profile.component.ts
@@ -1,8 +1,11 @@
+
 import { AuthService } from 'src/app/services/auth.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user';
 import { ProfileService } from 'src/app/services/profile.service';
 import { environment } from 'src/environments/environment';
+import { FoodTruck } from 'src/app/models/food-truck';
+import { FoodTruckService } from 'src/app/services/food-truck.service';
 
 @Component({
   selector: 'app-profile',
@@ -10,6 +13,13 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  @Input() truck: FoodTruck = new FoodTruck();
+  foodTrucks: FoodTruck[] = [];
+  selected: FoodTruck | null = null;
+  newFoodTruck: FoodTruck = new FoodTruck();
+  editFoodTruck: FoodTruck | null = null;
+  requests: Request | null = null;
+  foodTruck: FoodTruck | null = null;
   color1 = '#7F6C79';
   // private url = environment.baseUrl + "api/profile";
 
@@ -17,7 +27,8 @@ export class ProfileComponent implements OnInit {
   tempUser: User = new User();
   user: User | null = null;
 
-  constructor(private profileService: ProfileService, private authService: AuthService) { }
+
+  constructor(private profileService: ProfileService, private authService: AuthService, private truckSvc: FoodTruckService) { }
 
   ngOnInit(): void {
     this.authService.getLoggedInUser().subscribe({
@@ -40,7 +51,7 @@ export class ProfileComponent implements OnInit {
     if (!id) {
       return
     }
-    this.profileService.update(user, id).subscribe(
+    this.profileService.updateUser(user, id).subscribe(
       data => {
         //this.reload();
         //this.editProfile = null;
@@ -50,4 +61,44 @@ export class ProfileComponent implements OnInit {
     );
 
   }
-}
+  userOwnsTruck(): boolean {
+    let isOwner: boolean = false;
+    // for(this.user.role === foodTruckOwner)
+    {
+
+      if (this.user?.id === this.truck.user?.id) {
+    }
+      isOwner = true;
+      return isOwner;
+    }
+  }
+
+  displayFoodTrucks(foodTruck: FoodTruck) {
+    this.truckSvc.getUserTrucks().subscribe(
+      data =>{
+        this.foodTrucks= Object.assign({}, data)
+      },
+      err => console.log(err)
+    );
+  }
+  displayRequests(request: Request) {
+    this.requests = request;
+  }
+  updateFoodTruck(foodTruck: FoodTruck){
+    this.truckSvc.update(foodTruck).subscribe(
+      data => {
+        this.reload();
+        this.selected = null;
+        this.editFoodTruck = null;
+      }
+    );
+  }
+  reload() {
+    this.truckSvc.index().subscribe(
+      data => this.foodTrucks = data,
+      err => console.log(err)
+    );
+  }
+
+  }
+
--- ngFoodTruck/src/app/services/profile.service.ts
+++ ngFoodTruck/src/app/services/profile.service.ts
@@ -1,18 +1,24 @@
+import { FoodTruck } from './../models/food-truck';
 import { AuthService } from 'src/app/services/auth.service';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { User } from '../models/user';
+import { FoodTruckService } from './food-truck.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  private url = environment.baseUrl + 'api/users';
 
-  constructor(private http: HttpClient, private authServ: AuthService) { }
+  private url = environment.baseUrl + 'api/users';
+  private urlTruck = environment.baseUrl + 'api/trucks';
+  user: User = new User();
+
+
+  constructor(private http: HttpClient, private authServ: AuthService, private foodTruck: FoodTruck, private truckSvc: FoodTruckService) { }
 
   getHttpOptions() {
     let options = {
@@ -24,7 +30,7 @@ export class ProfileService {
     return options;
   }
 
-  update(updateUser: User, id: number) {
+  updateUser(updateUser: User, id: number) {
     return this.http.put<User>(this.url + "/" + updateUser.id, updateUser, this.getHttpOptions())
     .pipe(
       catchError((err: any) => {
@@ -34,4 +40,33 @@ export class ProfileService {
       })
     );
   }
+  create(foodTruck: FoodTruck, tid: number) {
+    // console.log(menuItem);
+    return this.http.post<FoodTruck>(this.urlTruck + '/trucks', this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError('KABOOM');
+      })
+    );
+  }
+
+  index(tid: number) {
+    return this.http.get<FoodTruck[]>(this.urlTruck + '/single/' + tid)
+    .pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError('KABOOM');
+      })
+    );
+  }
+  updateFoodTruck(updateFoodTruck: FoodTruck) {
+    //TODO: update routes!!!
+       return this.http.put<FoodTruck>((this.url + '/foodtruck/' + updateFoodTruck.id), updateFoodTruck, this.getHttpOptions()).pipe(
+         catchError((err: any) => {
+           console.log(err);
+           return throwError('KABOOM');
+         })
+       );
+     }
+
 }
