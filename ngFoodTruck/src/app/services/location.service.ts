72d8548dcde72c4b249de4e71de1779bbdd91615
breckiam <breckiam@gmail.com>
2022-05-31 08:42:08 -0600

    Service for location stubbed out

--- ngFoodTruck/src/app/services/location.service.ts
+++ ngFoodTruck/src/app/services/location.service.ts
@@ -1,9 +1,58 @@
+import { Location } from './../models/location';
+import { AuthService } from './auth.service';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocationService {
+  private url = environment.baseUrl + "api/locations";
 
-  constructor() { }
+  constructor(private http: HttpClient,
+    private authServ: AuthService) { }
+
+    getHttpOptions() {
+      let options = {
+        headers: {
+          Authorization: 'Basic ' + this.authServ.getCredentials(),
+          'X-Requested-With': 'XMLHttpRequest',
+        },
+      };
+      return options;
+    }
+
+    findByStreet(street: string, state: string, city: string, zip: string ) {
+      return this.http.get<Location>(this.url + '/search/' + street )
+      .pipe(
+        catchError((err: any) => {
+          console.log(err);
+          let newLocation: Location = new Location();
+          newLocation.street = street;
+          newLocation.state = state;
+          newLocation.city = city;
+          newLocation.zip = zip;
+          return this.http.post<Location>(this.url, location, this.getHttpOptions())
+          .pipe(
+            catchError((err: any) => {
+              console.log(err);
+              return throwError('KABOOM');
+            })
+          );
+        })
+      );
+    }
+
+    update(upLocation: Location, lid: number) {
+      return this.http.put<Location>((this.url + '/' + lid), upLocation, this.getHttpOptions()).pipe(
+        catchError((err: any) => {
+          console.log(err);
+          return throwError('KABOOM');
+        })
+      );
+    }
 }
